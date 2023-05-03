180d51e29b3571671d0015b36a58a57fa6aa9497
MO5AMED2002 <108222839+MO5AMED2002@users.noreply.github.com>
2023-05-03 11:02:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let photo = document.querySelector('.img');
+let container = document.querySelector('.container');
+function phones( phone ){
+    photo.src = phone ;
+}
+function colors( color ){
+    container.style.background = color ;
+}
